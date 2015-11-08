78309bc1efcfe252ab2498a4a7231a4a8788f25e
Justin Reyes <justin.regi.reyes@gmail.com>
2015-11-08 15:09:19 -0800

    added headshot2 to files

--- js/creative.js
+++ js/creative.js
@@ -26,12 +26,12 @@
     });
 
     // Fit Text Plugin for Main Header
-    $("h1").fitText(
-        1.2, {
-            minFontSize: '35px',
-            maxFontSize: '65px'
-        }
-    );
+    // $("h1").fitText(
+    //     1.2, {
+    //         minFontSize: '35px',
+    //         maxFontSize: '65px'
+    //     }
+    // );
 
     // $("h2").fitText(
     //     1.2, {

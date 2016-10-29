c008a286eb557d1ab6826d14447fcbaac65e3f0a
Justin Reyes <justin.regi.reyes@gmail.com>
2016-10-28 17:21:02 -0700

    Added rotating h1.i-am-a

--- public/js/section.js
+++ public/js/section.js
@@ -6,6 +6,7 @@ var Section = function(Page) {
 	this.hideSection = hideSection;
 	this.inViewListener = inViewListener;
 	this.animateAbout = false;
+	this.animIAmA = animIAmA;
 
 	this.inViewListener();
 }
@@ -27,29 +28,44 @@ function inViewListener() {
 function animSection($section) {
 	var _section = this;
 	var $titleWrapper = $section.find('div.title-wrapper');
-	var $hr = $titleWrapper.children('hr');
+	var $content = $section.find('div.content');
+	var $hr = $titleWrapper.find('hr');
 	$titleWrapper.animate({
 		opacity: 1,
 		bottom: 0
 		}, 500, "easeOutCubic", function() {
 			animateHr();
-		});
-
+		}
+	);
 
 	function animateHr() {
 		$hr.animate({
-			width: "70"
-			}, 750, "easeOutCirc", function() {
-				// animation complete
+			width: "100%"
+			}, 500, "easeOutCirc", function() {
+				animateContent();
 			}
 		);
-	}
+	};
+
+	function animateContent() {
+		$content.animate({
+				opacity: 1,
+				top: 0
+			}, 500, "easeOutCubic", function() {
+				var sectionId = $section.attr('id');
+				if(sectionId === "about") {
+					_section.animIAmA();
+				}
+			}
+		);	
+	};
 }
 
 function hideSection($section) {
 	var _section = this;
 	var $titleWrapper = $section.find('div.title-wrapper');
 	var $hr = $titleWrapper.children('hr');
+
 	$titleWrapper.animate({
 		opacity: 0,
 		bottom: 70
@@ -70,4 +86,77 @@ function hideAboutSection() {
 	var _section = this;
 	
 	hideSection($section);
+}
+
+function animIAmA() {
+	var $i_am_a = $("h1.i-am-a");
+	var $headerPrimary = $("h1.i-am-a.primary");
+	var $headerSecondary = $("h1.i-am-a.secondary");
+	var iAmWords = ["Code Tinkerer", "Sandwich Connoisseur", "Gamer", "Foodie", "Full Stack Web Developer", "Teachable"];
+	if($i_am_a.length) {
+		var switchCounter = 0;
+		var wordTracker = 0;
+		var iTimer = window.setInterval(function() {
+			wordTracker = switchWords(wordTracker, switchCounter, iAmWords, $headerPrimary, $headerSecondary);
+			switchCounter++;
+		}, 4000);
+	}
+};
+
+function switchWords(wordTracker, switchCounter, iAmWords, $headerPrimary, $headerSecondary) {
+
+
+	if(wordTracker < iAmWords.length) {
+		var word = iAmWords[wordTracker];
+		wordTracker++;
+	} else {
+		wordTracker = 0;
+		var word = iAmWords[wordTracker];
+	}
+
+	animWords(switchCounter, $headerPrimary, $headerSecondary, word);
+
+	return wordTracker;
+};
+
+function animWords(switchCounter, $headerPrimary, $headerSecondary, word) {
+	if(switchCounter % 2 == 0) {
+		$headerPrimary.animate({
+				top: 30,
+				opacity: 0
+			}, 500, "swing", 
+			function() {
+				// animation complete
+				$headerPrimary.css({top: "-30px"});
+				$headerPrimary.text(word)
+			}
+		);
+		$headerSecondary.animate({
+				top: 0,
+				opacity: 1
+			}, 500, "swing", 
+			function() {
+				// animation complete
+			}
+		);
+	} else {
+		$headerSecondary.animate({
+				top: 30,
+				opacity: 0
+			}, 500, "swing", 
+			function() {
+				// animation complete
+				$headerSecondary.css({top: "-30px"});
+				$headerSecondary.text(word)
+			}
+		);
+		$headerPrimary.animate({
+				top: 0,
+				opacity: 1
+			}, 500, "swing", 
+			function() {
+				// animation complete
+			}
+		);
+	}
 }
